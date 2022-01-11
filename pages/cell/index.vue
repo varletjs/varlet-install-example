3bb03764890074bab144036f41b19e95470bc39a
qytayh <13255238145@163.com>
2022-01-12 00:32:08 +0800

    feat: finish cell cases

--- pages/cell/index.vue
+++ pages/cell/index.vue
@@ -1,14 +1,45 @@
 <template>
+  <app-type>Basic Usage</app-type>
+  <var-cell>{{ content }}</var-cell>
+  <var-cell>{{ content }}</var-cell>
 
+  <app-type>Show Icon</app-type>
+  <var-cell icon="fire" :title="content">
+    <template #extra>
+      <var-icon name="information" />
+    </template>
+  </var-cell>
+  <var-cell icon="fire" :title="content">
+    <template #extra>
+      <var-icon name="information" />
+    </template>
+  </var-cell>
+
+  <app-type>Show Description</app-type>
+  <var-cell icon="fire" :title="content" :desc="description">
+    <template #extra>
+      <var-icon name="information" />
+    </template>
+  </var-cell>
+  <var-cell :title="content" :desc="description" />
+
+  <app-type>Show Border</app-type>
+  <var-cell border> {{ content }} </var-cell>
+  <var-cell border> {{ content }} </var-cell>
 </template>
-<script lang='ts'>
-import {defineNuxtComponent} from '#app'
+<script lang="ts">
+import { defineNuxtComponent } from '#app'
+
 import AppType from '~/components/appType.vue'
 export default defineNuxtComponent({
-  components:{
-    AppType
-  }
-
+  components: {
+    AppType,
+  },
+  setup() {
+    const content = 'This is Cell.'
+    const description = 'Description'
+    return { content, description }
+  },
 })
 </script>
 <style></style>
