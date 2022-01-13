d98a8f90bb81e6ccc905ad2917a1e31091f9530d
wzhaofei <wangzf@wellnkiot.com>
2022-01-13 10:44:30 +0800

    feat: back-top cases

---
+++ pages/back-top/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="example">
+    <var-cell v-for="list in lists" :key="list">Scroll to bottom {{ list }}</var-cell>
+    <var-back-top :duration="300" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineNuxtComponent } from '#app'
+export default defineNuxtComponent({
+  setup() {
+    const lists = [...Array(100).keys()]
+    return {
+      lists,
+    }
+  },
+})
+</script>
