7564858372f4b7119c84855ea4b7554255170a72
allenl <liangle8090@gmail.com>
2022-01-13 12:00:59 +0800

    feat: add card

---
+++ pages/card/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <app-type>Basic Usage</app-type>
+  <var-card :title="title" :description="description" />
+
+  <app-type>Show Subtitle</app-type>
+  <var-card :title="title" :subtitle="subtitle" :description="description" />
+
+  <app-type>Show Image</app-type>
+  <var-card :title="title" :subtitle="subtitle" :description="description" :src="src" />
+
+  <app-type>Use Slot</app-type>
+  <var-card :title="title" :subtitle="subtitle" :description="description" :src="src">
+    <template #extra>
+      <var-space>
+        <var-button type="primary">Use Button</var-button>
+        <var-button type="warning">Use Button</var-button>
+      </var-space>
+    </template>
+  </var-card>
+
+  <app-type>Ripple Effect</app-type>
+  <var-card :title="title" :subtitle="subtitle" :description="description" ripple />
+</template>
+
+<script>
+export default defineNuxtComponent({
+  setup() {
+    return {
+      title: 'Little Prince',
+      subtitle: 'Little prince from',
+      src: 'https://varlet.gitee.io/varlet-ui/cat.jpg',
+      description:
+        'It took me a long time to learn where he came from. The little prince, whoasked me so many questions, never seemed to hear the ones I asked him. Itwas from words dropped by chance that, little by little, everything wasrevealed to me.',
+    }
+  },
+})
+</script>
