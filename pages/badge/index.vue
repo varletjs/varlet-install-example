53d0e51122605fdd2a231fc2a7433c1a4759be19
BambooSword <dengliming@tngdigital.com.my>
2022-01-13 22:23:48 +0800

    feat: add var-space  to beautify the page (#13)

--- pages/badge/index.vue
+++ pages/badge/index.vue
@@ -1,50 +1,56 @@
 <template>
   <div class="example">
     <app-type>Theme Color Badge</app-type>
-    <var-badge />
-    <var-badge type="primary" />
-    <var-badge type="info" />
-    <var-badge type="warning" />
-    <var-badge type="success" />
-    <var-badge type="danger" />
+    <var-space>
+      <var-badge />
+      <var-badge type="primary" />
+      <var-badge type="info" />
+      <var-badge type="warning" />
+      <var-badge type="success" />
+      <var-badge type="danger" />
+    </var-space>
 
     <app-type>Dot Badge</app-type>
     <var-badge dot type="danger" />
 
     <app-type>Custom Content Badge</app-type>
-    <var-badge type="danger" value="badge" />
-    <var-badge type="danger" value="hot" />
-    <var-badge type="danger" value="66" />
+    <var-space>
+      <var-badge type="danger" value="badge" />
+      <var-badge type="danger" value="hot" />
+      <var-badge type="danger" value="66" />
+    </var-space>
 
     <app-type>Maximum</app-type>
-    <var-badge type="danger" :value="value" :max-value="maxValue" />
-    <var-badge type="danger" :value="value1" :max-value="maxValue" />
-
+    <var-space>
+      <var-badge type="danger" :value="value" :max-value="maxValue" />
+      <var-badge type="danger" :value="value1" :max-value="maxValue" />
+    </var-space>
     <app-type>Different Positioning Badges</app-type>
     <div class="example__position">
-      <var-badge type="danger" position="right-top">
-        <var-chip plain :round="false" color="#009688">The Upper Right</var-chip>
-      </var-badge>
-      <br />
-      <var-badge type="danger" position="right-bottom">
-        <var-chip plain :round="false" color="#009688">The Lower Right</var-chip>
-      </var-badge>
-      <br />
-      <var-badge type="danger" position="left-top">
-        <var-chip plain :round="false" color="#009688">The Upper Left</var-chip>
-      </var-badge>
-      <br />
-      <var-badge type="danger" position="left-bottom">
-        <var-chip plain :round="false" color="#009688">The Lower Left</var-chip>
-      </var-badge>
+      <var-space direction="column" size="large">
+        <var-badge type="danger" position="right-top">
+          <var-chip plain :round="false" color="#009688">The Upper Right</var-chip>
+        </var-badge>
+        <var-badge type="danger" position="right-bottom">
+          <var-chip plain :round="false" color="#009688">The Lower Right</var-chip>
+        </var-badge>
+        <var-badge type="danger" position="left-top">
+          <var-chip plain :round="false" color="#009688">The Upper Left</var-chip>
+        </var-badge>
+        <var-badge type="danger" position="left-bottom">
+          <var-chip plain :round="false" color="#009688">The Lower Left</var-chip>
+        </var-badge>
+      </var-space>
     </div>
 
     <app-type>Whether To Display The Badge</app-type>
-    <var-button @click="handleChange"> Click To Change The State </var-button>
-    <var-badge type="danger" position="right-top" :hidden="hidden">
-      <var-chip plain :round="false" color="#009688">Badge</var-chip>
-    </var-badge>
-
+    <var-space>
+      <var-button @click="handleChange"> Click To Change The State </var-button>
+      <var-badge type="danger" position="right-top" :hidden="hidden">
+        <var-chip plain :round="false" color="#009688">Badge</var-chip>
+      </var-badge>
+    </var-space>
+		
     <app-type>Custom Badge Colors</app-type>
     <var-badge color="#6200ea" position="right-top">
       <var-chip plain :round="false" color="#009688">Badge</var-chip>
@@ -83,8 +89,8 @@ export default {
   margin-bottom: 8px;
 }
 .example {
-	&__position {
-		margin-left: 10px;
-	}
+  &__position {
+    margin-left: 10px;
+  }
 }
 </style>
