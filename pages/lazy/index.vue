c8418ebccf3e46e23b7f655ed95e015cab3ac654
chunhuigao <18606710205@163.com>
2022-01-15 01:37:43 +0800

    feat: add example for lazy (#19)
    * feat: add example for lazy
    
    * feat: modify picture path
    
    * feat: modify picture path

---
+++ pages/lazy/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="example">
+    <app-type> Basic Usage </app-type>
+    <var-space>
+      <img class="cat" v-lazy="lazy" />
+      <img class="cat" v-lazy="lazy" />
+      <img class="cat" v-lazy="lazy" />
+      <img class="cat" v-lazy="lazy" />
+      <img class="cat" v-lazy="lazy" />
+      <img class="cat" v-lazy="lazy" />
+    </var-space>
+
+    <app-type> Background Image Lazy Load </app-type>
+    <div class="cat height" v-lazy:background-image="lazy"></div>
+
+    <app-type> Inline Attributes </app-type>
+    <img
+      class="cat maxHeight"
+      v-lazy="lazy1"
+      lazy-loading="https://varlet.gitee.io/varlet-ui/varlet_icon.png"
+      lazy-error="https://varlet.gitee.io/varlet-ui/cover.jpg"
+      lazy-attempt="3"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineNuxtComponent } from '#app'
+import AppType from '~/components/appType.vue'
+export default defineNuxtComponent({
+  components: {
+    AppType,
+  },
+  setup() {
+    const data = reactive({
+      lazy: 'https://varlet.gitee.io/varlet-ui/cat.jpg',
+    })
+    return { ...toRefs(data) }
+  },
+})
+</script>
+<style lang="less" scoped>
+.cat {
+  width: 100%;
+  object-fit: cover;
+  background-size: cover;
+  pointer-events: none;
+}
+.height {
+  height: 200px;
+}
+.maxHeight {
+  max-height: 200px;
+}
+</style>
