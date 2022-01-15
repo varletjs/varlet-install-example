00ed6322f6a308acb1a2f67bfe21580b1b932aa4
lin <linyuhong@defencedriver.com>
2022-01-15 16:51:20 +0800

    feat: add space cases

--- pages/space/index.vue
+++ pages/space/index.vue
@@ -7,6 +7,38 @@
       <var-button>Button3</var-button>
     </var-space>
 
+    <app-type>Align Center</app-type>
+    <var-space align="center" :size="[20, 20]">
+      <var-button size="mini">Button1</var-button>
+      <var-button size="small">Button2</var-button>
+      <var-button size="normal">Button3</var-button>
+      <var-button size="large">Button3</var-button>
+    </var-space>
+
+    <app-type>Align Start</app-type>
+    <var-space align="start" :size="[20, 20]">
+      <var-button size="mini">Button1</var-button>
+      <var-button size="small">Button2</var-button>
+      <var-button size="normal">Button3</var-button>
+      <var-button size="large">Button3</var-button>
+    </var-space>
+
+    <app-type>Align End</app-type>
+    <var-space align="end" :size="[20, 20]">
+      <var-button size="mini">Button1</var-button>
+      <var-button size="small">Button2</var-button>
+      <var-button size="normal">Button3</var-button>
+      <var-button size="large">Button3</var-button>
+    </var-space>
+
+    <app-type>Align Baseline</app-type>
+    <var-space align="baseline" :size="[20, 20]">
+      <var-button size="mini">Button1</var-button>
+      <var-button size="small">Button2</var-button>
+      <var-button size="normal">Button3</var-button>
+      <var-button size="large">Button3</var-button>
+    </var-space>
+
     <app-type>Vertical</app-type>
     <var-space direction="column" size="large">
       <var-button>Button1</var-button>
@@ -24,7 +56,7 @@
       <var-button>Button6</var-button>
     </var-space>
 
-    <app-type>Right Align</app-type>
+    <app-type>Justify Right</app-type>
     <var-space justify="end">
       <var-button>Button1</var-button>
       <var-button>Button2</var-button>
@@ -36,7 +68,7 @@
       <var-button>Button2</var-button>
     </var-space>
 
-    <app-type>Align Center</app-type>
+    <app-type>Justify Center</app-type>
     <var-space justify="center">
       <var-button>Button1</var-button>
       <var-button>Button2</var-button>
@@ -46,6 +78,16 @@
     <var-space justify="space-between">
       <var-button>Button1</var-button>
       <var-button>Button2</var-button>
+    </var-space>
+
+    <app-type>No Wrap</app-type>
+    <var-space :wrap="false">
+      <var-button>Button1</var-button>
+      <var-button>Button2</var-button>
+      <var-button>Button3</var-button>
+      <var-button>Button4</var-button>
+      <var-button>Button5</var-button>
+      <var-button>Button6</var-button>
     </var-space>
   </div>
 </template>
