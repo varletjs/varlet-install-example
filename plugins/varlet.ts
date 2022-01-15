f1e4c66c63b3f2b356c8ef199aca7ebf2696332d
qytayh <13255238145@163.com>
2022-01-16 01:55:44 +0800

    chore(locale): add locale enUS lang

--- plugins/varlet.ts
+++ plugins/varlet.ts
@@ -1,8 +1,13 @@
 import { defineNuxtPlugin } from '#app'
 import '@varlet/ui/es/style'
 import '@varlet/touch-emulator'
-import VarLet from '@varlet/ui'
+import VarLet,{ Locale } from '@varlet/ui'
+import enUS from '@varlet/ui/es/locale/en-US'
+
+
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VarLet)
+  Locale.add('en-US', enUS)
+  Locale.use('en-US')
 })
