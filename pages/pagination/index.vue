22addc69842ceca4883bfabc8148ff8342a03e08
BambooSword <herodreamdeng@gmail.com>
2022-01-19 11:13:53 +0800

    feat: finish popup and pagination page (#28)
    * fix: use defineNuxtComponent
    
    * fix: format
    
    * feat: finish popup and pagination page
    
    * fix: popup and pagination page- add defineNextComponent and use variable to link text

---
+++ pages/pagination/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="example">
+    <app-type>Basic Usage</app-type>
+    <div class="transformer">
+      <var-pagination :current="3" :total="120" />
+    </div>
+    <app-type>Hide Size Changer</app-type>
+    <div class="transformer">
+      <var-pagination :current="3" :total="120" :show-size-changer="false" />
+    </div>
+    <app-type>Show Total</app-type>
+    <div class="transformer">
+      <var-pagination :current="3" :total="120" :show-total="(total) => ` ${total} items`" />
+    </div>
+    <app-type>Disabled</app-type>
+    <div class="transformer">
+      <var-pagination :current="3" :total="120" disabled />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import AppType from '~/components/AppType'
+import { defineNuxtComponent } from '#app'
+
+export default defineNuxtComponent({
+  name: 'PaginationExample',
+  components: {
+    AppType,
+  },
+})
+</script>
+
+<style lang="less" scoped>
+.transformer {
+  transform: translateX(-10px);
+}
+</style>
