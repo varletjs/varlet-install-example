3b91bf0ef3404eb4871de816ccd01292d3670e33
qytayh <13255238145@163.com>
2022-01-19 10:42:59 +0800

    style: rename some page folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,10 @@ import { defineNuxtConfig } from 'nuxt3'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   buildModules: ['@pinia/nuxt'],
+  router: {
+    scrollBehavior (to, from, savedPosition) {
+      return { x: 0, y: 0 }
+    }
+  }
 })
 
