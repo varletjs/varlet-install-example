3482e038743c3f1ac1bd3e5eada038b2ea492de6
qytayh <13255238145@163.com>
2022-01-19 10:56:07 +0800

    feat: auto scrollTop after router change

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,6 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  buildModules: ['@pinia/nuxt'],
-  router: {
-    scrollBehavior (to, from, savedPosition) {
-      return { x: 0, y: 0 }
-    }
-  }
+  buildModules: ['@pinia/nuxt']
 })
 
---
+++ plugins/router.ts
@@ -0,0 +1,11 @@
+import {defineNuxtPlugin} from "#app";
+
+
+export default defineNuxtPlugin((nuxtApp) => {
+    if(process.client){
+        nuxtApp.$router.afterEach((to, from, next) => {
+            document.body.scrollTop = 0
+            document.documentElement.scrollTop = 0
+        });
+    }
+})
