35679205fdf7f14039e6921fe60dabcef0c08b75
qytayh <13255238145@163.com>
2022-01-25 22:31:11 +0800

    style(image): change srcurl to a variable

--- pages/image/index.vue
+++ pages/image/index.vue
@@ -1,55 +1,48 @@
 <template>
   <div class="example">
     <app-type>Basic Usage</app-type>
-    <var-image src="https://varlet.gitee.io/varlet-ui/cat.jpg" />
+    <var-image :src="picUrl" />
 
     <app-type>Fit Mode</app-type>
     <var-space>
       <div class="fit-item">
-        <var-image width="85px" height="85px" src="https://varlet.gitee.io/varlet-ui/cat.jpg" />
+        <var-image width="85px" height="85px" :src="picUrl" />
         <div>fill</div>
       </div>
 
       <div class="fit-item">
-        <var-image width="85px" height="85px" fit="cover" src="https://varlet.gitee.io/varlet-ui/cat.jpg" />
+        <var-image width="85px" height="85px" fit="cover" :src="picUrl" />
         <div>cover</div>
       </div>
 
       <div class="fit-item">
-        <var-image width="85px" height="85px" fit="contain" src="https://varlet.gitee.io/varlet-ui/cat.jpg" />
+        <var-image width="85px" height="85px" fit="contain" :src="picUrl" />
         <div>contain</div>
       </div>
 
       <div class="fit-item">
-        <var-image width="85px" height="85px" fit="none" src="https://varlet.gitee.io/varlet-ui/cat.jpg" />
+        <var-image width="85px" height="85px" fit="none" :src="picUrl" />
         <div>none</div>
       </div>
 
       <div class="fit-item">
-        <var-image width="85px" height="85px" fit="scale-down" src="https://varlet.gitee.io/varlet-ui/cat.jpg" />
+        <var-image width="85px" height="85px" fit="scale-down" :src="picUrl" />
         <div>scale-down</div>
       </div>
     </var-space>
 
     <app-type>Set Radius</app-type>
     <var-space>
-      <var-image
-        width="85px"
-        height="85px"
-        fit="cover"
-        :radius="10"
-        src="https://varlet.gitee.io/varlet-ui/cat.jpg"
-        style="margin-right: 10px"
-      />
+      <var-image width="85px" height="85px" fit="cover" :radius="10" :src="picUrl" style="margin-right: 10px" />
 
-      <var-image width="85px" height="85px" fit="cover" radius="50%" src="https://varlet.gitee.io/varlet-ui/cat.jpg" />
+      <var-image width="85px" height="85px" fit="cover" radius="50%" :src="picUrl" />
     </var-space>
 
     <app-type>Use Ripple</app-type>
-    <var-image ripple src="https://varlet.gitee.io/varlet-ui/cat.jpg" />
+    <var-image ripple :src="picUrl" />
 
     <app-type>Use LazyLoad</app-type>
-    <var-image lazy src="https://varlet.gitee.io/varlet-ui/cat.jpg" />
+    <var-image lazy :src="picUrl" />
   </div>
 </template>
 
@@ -61,6 +54,13 @@ export default defineNuxtComponent({
   name: 'ImageExample',
   components: {
     AppType,
+  },
+  setup() {
+    const picUrl = 'https://varlet.gitee.io/varlet-ui/cat.jpg'
+
+    return {
+      picUrl,
+    }
   },
 })
 </script>
