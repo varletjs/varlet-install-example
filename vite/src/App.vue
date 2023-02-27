5a42fb1f605691c488d9120159e75ea7ff664902
haoziqaq <357229046@qq.com>
2023-02-27 13:54:17 +0800

    style: clear code

--- vite/src/App.vue
+++ vite/src/App.vue
@@ -1,23 +1,17 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
 function handleClick() {
   Snackbar('Hello')
 }
-
-const value = ref('')
 </script>
 
 <template>
   <var-tooltip content="Say Hello">
-    <var-button @click="handleClick" v-ripple="{ disabled: 123 }">
+    <var-button @click="handleClick">
       Say Hello
       <var-icon name="xml" />
       <var-icon namespace="var-ext-icon" name="a" />
     </var-button>
   </var-tooltip>
-
-  <var-input type="number" v-model="value" />
 </template>
 
 <style>
