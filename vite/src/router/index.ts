c31fb7ba4bf84f2dd46f7b659d7b31c0204a8df5
haoziqaq <357229046@qq.com>
2023-04-15 01:07:24 +0800

    chore: clear code

--- vite/src/router/index.ts
+++ vite/src/router/index.ts
@@ -1,8 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from '~pages'
 
-console.log(routes)
-
 export default createRouter({
   history: createWebHistory(),
   routes: [
