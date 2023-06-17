703aa49b25f585071150f8045bfbc1469747a09f
haoziqaq <357229046@qq.com>
2023-06-18 02:01:24 +0800

    chore: bump

--- nuxt/components.d.ts
+++ nuxt/components.d.ts
@@ -12,7 +12,9 @@ declare module '@vue/runtime-core' {
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     VarButton: typeof import('@varlet/ui')['_ButtonComponent']
-    VarDialog: typeof import('@varlet/ui')['_DialogComponent']
+    VarPopup: typeof import('@varlet/ui')['_PopupComponent']
+    VarSwipe: typeof import('@varlet/ui')['_SwipeComponent']
+    VarSwipeItem: typeof import('@varlet/ui')['_SwipeItemComponent']
     VarTooltip: typeof import('@varlet/ui')['_TooltipComponent']
   }
 }
--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,13 +1,31 @@
 <script setup lang="ts">
+const show = ref(false)
+
 function handleClick() {
-  Snackbar('Hello')
+  show.value = true
 }
 </script>
 
 <template>
-  <var-tooltip content="Say Hello">
-    <var-button type="primary" @click="handleClick">Say Hello</var-button>
+  <var-tooltip content="Show Popup">
+    <var-button type="primary" @click="handleClick">Show Popup</var-button>
   </var-tooltip>
+
+  <var-popup v-model:show="show">
+    <div class="popup-example-block">
+      <var-swipe class="swipe-example" :autoplay="1000">
+        <var-swipe-item>
+          <img class="swipe-example-image" src="https://varlet.gitee.io/varlet-ui/cat.jpg" />
+        </var-swipe-item>
+        <var-swipe-item>
+          <img class="swipe-example-image" src="https://varlet.gitee.io/varlet-ui/cat2.jpg" />
+        </var-swipe-item>
+        <var-swipe-item>
+          <img class="swipe-example-image" src="https://varlet.gitee.io/varlet-ui/cat3.jpg" />
+        </var-swipe-item>
+      </var-swipe>
+    </div>
+  </var-popup>
 </template>
 
 <style>
@@ -15,5 +33,21 @@ body {
   transition: background-color 0.25s;
   color: var(--color-text);
   background-color: var(--color-body);
+}
+
+.popup-example-block {
+  padding: 20px 24px;
+  width: 250px;
+}
+
+.swipe-example {
+  height: 160px;
+}
+
+.swipe-example-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  pointer-events: none;
 }
 </style>
