bc2755c470014d380c27bfe703f7f6f6943c18ea
ADNY <66500121+ErKeLost@users.noreply.github.com>
2023-12-03 15:32:54 +0800

    feat: support farm (#67)

---
+++ farm/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <var-tooltip content="Hello">
+    <var-button type="primary" >Hello</var-button>
+  </var-tooltip>
+</template>
+
+<style>
+body {
+  transition: background-color 0.25s;
+  color: var(--color-text);
+  background-color: var(--color-body);
+}
+</style>
