2b7c92d5b46ea22dc1f1c35f00063f633cd56bd8
haoziqaq <357229046@qq.com>
2024-04-07 14:16:12 +0800

    feat: support varlet module

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,24 +1,3 @@
-import components from 'unplugin-vue-components/vite'
-import autoImport from 'unplugin-auto-import/vite'
-import { VarletImportResolver } from '@varlet/import-resolver'
-
 export default defineNuxtConfig({
-  vite: {
-    ssr: {
-      noExternal: ['@varlet/ui']
-    },
-
-    plugins: [
-      components({
-        resolvers: [VarletImportResolver()],
-        dts: true
-      }),
-      
-      autoImport({
-        resolvers: [VarletImportResolver({ autoImport: true })],
-        dts: true
-      })
-    ]
-  }
+  modules: ["@varlet/nuxt"]
 })
-
