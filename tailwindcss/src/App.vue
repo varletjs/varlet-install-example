bd45a4c83355a5e6a908bd0fbb6634550144b932
chouchouji <1305974212@qq.com>
2024-07-20 08:31:02 +0800

    feat(tailwindcss): add example about the usage of hsl

--- tailwindcss/src/App.vue
+++ tailwindcss/src/App.vue
@@ -2,4 +2,5 @@
   <div class="text-on-primary bg-primary text-md">hello</div>
   <div class="text-on-primary-container bg-primary-container text-lg">world</div>
   <div class="bg-primary sm:bg-info md:bg-warning lg:bg-danger xl:bg-success">varlet</div>
+  <div class="bg-hsl-primary/50 text-hsl-text/70">tailwindcss</div>
 </template>
